686af194fec455d9834479b789d8c81bd19e504b
TheKushStyle <Frank.boersma@hotmail.com>
2014-10-20 22:34:41 +0200

    Now Checks for recalling!

--- EmoteSpammer/Program.cs
+++ EmoteSpammer/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿#region
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -7,7 +8,7 @@ using System.Threading;
 using SharpDX;
 using LeagueSharp;
 using LeagueSharp.Common;
-
+#endregion
 
 namespace EmoteSpammer
 {
@@ -15,7 +16,6 @@ namespace EmoteSpammer
     {
         private static Menu Config;
         private double tick;
-        private double tickc;
 
         private static void Main(string[] args)
         {
@@ -27,7 +27,7 @@ namespace EmoteSpammer
             Config = new Menu("EmoteSpammer", "EmoteSpammer", true);
             Config.AddSubMenu(new Menu("On/Off", "On/Off"));
             Config.SubMenu("On/Off").AddItem(new MenuItem("EmotePress", "Emote On Key press")).SetValue(new KeyBind(32, KeyBindType.Press));
-            Config.SubMenu("On/Off").AddItem(new MenuItem("EmoteToggable", "Toggable Emote")).SetValue(new KeyBind('h', KeyBindType.Toggle));
+            Config.SubMenu("On/Off").AddItem(new MenuItem("EmoteToggable", "Toggleable Emote")).SetValue(new KeyBind('h', KeyBindType.Toggle));
             Config.SubMenu("On/Off").AddItem(new MenuItem("Type", "Wich Emote to spam?")).SetValue(new Slider(1, 3, 1));
 
             Config.AddToMainMenu();
@@ -45,9 +45,10 @@ namespace EmoteSpammer
         {
             double tick = 0;
             tick = TimeSpan.FromSeconds(Environment.TickCount).Minutes;
+
+            if (ObjectManager.Player.HasBuff("Recall")) return;
             
-            
-            Game.PrintChat(tick.ToString());
+            //Game.PrintChat(tick.ToString());
             {
                 if ((Config.Item("EmotePress").GetValue<KeyBind>().Active) && tick == 59 )
                 {
@@ -55,29 +56,12 @@ namespace EmoteSpammer
                 }
                 if (Config.Item("EmoteToggable").GetValue<KeyBind>().Active && tick == 59)
                 {
-                    SPAMT();
+                    SPAM();
                 }
             }
         }
 
-        private static void SPAMT()
-        {
-            if ((Config.Item("Type").GetValue<Slider>().Value) == 1)
-            {
-                Packet.C2S.Emote.Encoded(new Packet.C2S.Emote.Struct(2)).Send();
-                Packet.C2S.Move.Encoded(new Packet.C2S.Move.Struct(Game.CursorPos.X, Game.CursorPos.Y)).Send();
-            }
-            if ((Config.Item("Type").GetValue<Slider>().Value) == 2)
-            {
-                Packet.C2S.Emote.Encoded(new Packet.C2S.Emote.Struct(1)).Send();
-                Packet.C2S.Move.Encoded(new Packet.C2S.Move.Struct(Game.CursorPos.X, Game.CursorPos.Y)).Send();
-            }
-            if ((Config.Item("Type").GetValue<Slider>().Value) == 3)
-            {
-                Packet.C2S.Emote.Encoded(new Packet.C2S.Emote.Struct(3)).Send();
-                Packet.C2S.Move.Encoded(new Packet.C2S.Move.Struct(Game.CursorPos.X, Game.CursorPos.Y)).Send();
-            }
-        } 
+        
         private static void SPAM()
         {
             
