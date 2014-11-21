26dd4fd9ba9c4125a48435dec3d2bc5acff617db
TheKushStyle <TheKushStyle@users.noreply.github.com>
2014-11-21 14:53:53 +0100

    Code  Cleanup

--- CustomOverlay/Program.cs
+++ CustomOverlay/Program.cs
@@ -1,4 +1,4 @@
-﻿#region
+#region
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +8,9 @@ using System.Drawing;
 using LeagueSharp;
 using LeagueSharp.Common;
 using SharpDX;
-
-
+using Color = System.Drawing.Color;
 #endregion
+
 namespace CustomOverlay
 {
     class Program
@@ -20,31 +20,33 @@ namespace CustomOverlay
 
         static void Main(string[] args)
         {
-            CustomEvents.Game.OnGameLoad += eventArgs =>
-            {
-                config = new Menu("Custom Overlay", "Custom Overlay", true);
-                config.AddItem(new MenuItem("slider", "Choose Overlay").SetValue(new Slider(1, 1,42)));
-
-                sprite = new Render.Sprite(Properties.Resources.hud_1, new Vector2(1, 1));
-                sprite.Add(0);
-                Game.PrintChat("<font color='#FF00BF'>Custom Overlay Loaded By</font> <font color='#FF0000'>The</font><font color='#FFFF00'>Kush</font><font color='#40FF00'>Style</font>");
-            };
-
-            Game.OnGameUpdate += eventArgs =>
-            {
-                UpdateImage((Bitmap)Properties.Resources.ResourceManager.GetObject(string.Format("hud_{0}", config.Item("slider").GetValue<Slider>().Value))); // try to update image
-            };
+            CustomEvents.Game.OnGameLoad += Game_OnGameLoad;
         }
 
-        static void UpdateImage(Bitmap bitmap)
+        private static void Game_OnGameLoad(EventArgs args)
+        {
+
+            config = new Menu("Custom Overlay", "Custom Overlay", true);
+            config.AddItem(new MenuItem("slider", "Choose Overlay").SetValue(new Slider(1, 1, 42)));
+
+            sprite = new Render.Sprite(Properties.Resources.hud_1, new Vector2(1, 1));
+            sprite.Add(0);
+
+            Game.PrintChat("<font color='#FF00BF'>Custom Overlay Loaded By</font> <font color='#FF0000'>The</font><font color='#FFFF00'>Kush</font><font color='#40FF00'>Style</font>");
+            
+            Game.OnGameUpdate += OnGameUpdate;
+        }
+
+        private static void OnGameUpdate(EventArgs args)
+        {
+            UpdateImage((Bitmap)Properties.Resources.ResourceManager.GetObject(string.Format("hud_{0}", config.Item("slider").GetValue<Slider>().Value))); 
+        }
+
+        private static void UpdateImage(Bitmap bitmap)
         {
             if (sprite.Bitmap == bitmap)
                 return;
-
-            sprite.UpdateTextureBitmap(bitmap); 
+            sprite.UpdateTextureBitmap(bitmap);
         }
     }
- 
 }
-
-        
