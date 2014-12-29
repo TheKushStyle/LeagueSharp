8917d3101d81ae42401ad4e454bd17590f55ad88
TheKushStyle <Frank.boersma@hotmail.com>
2014-12-29 16:53:40 +0100

    Release

---
+++ JustMute/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeagueSharp;
+using LeagueSharp.Common;
+
+namespace JustMute
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            CustomEvents.Game.OnGameLoad += Game_OnGameLoad;
+        }
+
+        private static void Game_OnGameLoad(EventArgs args)
+        {
+            Game.Say("/mute all");
+        }
+    }
+}
